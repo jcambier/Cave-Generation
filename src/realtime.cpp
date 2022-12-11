1ec6d4466adf143c1deec353e1901008ef8e307e
Jared <jared_cambier@brown.edu>
2022-12-11 11:16:40 -0500

    before combining

--- src/realtime.cpp
+++ src/realtime.cpp
@@ -83,7 +83,7 @@ void Realtime::initializeGL() {
     glBindTexture(GL_TEXTURE_2D, m_stone_texture);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_image.width(), m_image.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, m_image.bits());
     glGenerateMipmap(GL_TEXTURE_2D);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glBindTexture(GL_TEXTURE_2D, 0);
     glUseProgram(m_shader);
